7c469ddc9191c050857bc604eb6236e684e0504b
DaDaCheng <shicheng1995@hotmail.com>
2019-11-12 23:23:41 +0100

    cluster3

--- basepolicy.sh
+++ basepolicy.sh
@@ -4,5 +4,6 @@
 #SBATCH --time=10:30:30
 
 source activate tf
-./simphas/playmf.py --learning_rate 0.01 --n_episode 500 --h_speed $1 --s_speed $2 --opt $3 --seeds $4 --out $5 --vlag 0
+./simphas/playmf.py --learning_rate 0.0003 --n_episode 1000 --episode 400 --GAMMA 0.999 --h_speed $1 --s_speed $2 --opt $3 --seeds $4 --out $5 --vlag 0
 source deactivate
+
